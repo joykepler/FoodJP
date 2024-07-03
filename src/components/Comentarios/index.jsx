8da34c0a2eae9f9c3686d663e74ce052fc3baa46
joykepler <joyjoykepler@gmail.com>
2024-07-03 19:59:40 -0300

    'responsive'

--- src/components/Comentarios/index.jsx
+++ src/components/Comentarios/index.jsx
@@ -5,9 +5,9 @@ import Perfil2 from '../../assets/perfil2.svg'
 
 const Comentarios = () => {
     return (
-        <div className="populares">
+        <div className="comentarios">
             <h1>Comentários</h1>
-            <div className="populares__cards">
+            <div className="comentarios__cards">
                 <CardComents imagem={Perfil1} texto="Daniele Almeida" texto2="Ótimo serviço! Encantada com a qualidade dos pratos."/>
                 <CardComents imagem={Perfil2} texto="Ricardo França" texto2="Não é apenas a comida excelente, o serviço torna a experiência especial."/>
             </div>
