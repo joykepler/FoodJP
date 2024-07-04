1a752630c38f2c43dfcce43e25df92769b763997
joykepler <joyjoykepler@gmail.com>
2024-07-03 21:06:12 -0300

    'responsividade'

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,19 +1,19 @@
-const Button = ({texto, largura, altura}) => { 
+const Button = ({ texto, largura, altura, fontSize }) => {
     const buttonStyle = {
         width: largura,
         height: altura,
-        padding: '12px 40px',
         borderRadius: '60px',
         border: 'none',
         backgroundColor: '#FF0000',
         color: '#FFFFFF',
         fontWeight: '500',
-        fontSize: '20px',
+        fontSize: fontSize || '20px',
         fontFamily: '"Poppins", sans-serif'
-    }
+    };
+
     return (
         <button style={buttonStyle}>{texto}</button>
-    )
-}
+    );
+};
 
 export default Button;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,33 @@
-import './style.css'
-import Logo from '../../assets/Logo.svg'
-import Button from '../Button'
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import Logo from '../../assets/Logo.svg';
+import Button from '../Button';
 
-const Header = () => { 
+const Header = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const headerButtonStyle = {
+        width: windowWidth <= 767 ? '100px' : '170px',
+        fontSize: windowWidth <= 767 ? '15px' : 'initial',
+        texto: windowWidth <= 767 ? 'Baixar' : 'Baixar o App',
+    };
+
     return (
-    <header>
-        <img src={Logo} alt="Logo" />
-        <nav>
-            <a href="#">Início</a>
-            <a href="#">Sobre nós</a>
-            <a href="#">Populares</a>
-        </nav>
-        <Button texto="Baixar o App" />
-        <div className='menu-hamburguer'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-            </svg>
-        </div>
-    </header>
+        <header>
+            <img src={Logo} alt="Logo" />
+            <nav>
+                <a href="#">Início</a>
+                <a href="#">Sobre nós</a>
+                <a href="#">Populares</a>
+            </nav>
+            <Button texto={headerButtonStyle.texto} largura={headerButtonStyle.width} altura="50px" fontSize={headerButtonStyle.fontSize}/>
+        </header>
     )
 }
 
