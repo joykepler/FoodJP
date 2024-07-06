044faaf3a4dfe3a3dc808d87f72a502e38cc07fd
joykepler <joyjoykepler@gmail.com>
2024-07-06 15:17:51 -0300

    card

--- src/components/Populares/index.jsx
+++ src/components/Populares/index.jsx
@@ -11,7 +11,7 @@ const Populares = () => {
             <div className="populares__cards">
                 <Card imagem={Img1Card} texto="Ramen de frango" texto2="Um prato reconfortante com caldo saboroso e pedaços tenros de frango"/>
                 <Card imagem={Img2Card} texto="Ramen apimentado" texto2="Ramen apimentado é ideal para quem gosta de uma explosão de sabor picante em cada garfada."/>
-                <Card imagem={Img3Card} texto="Ramen tradicional"texto2="Ramen tradicional traz a autenticidade da culinária japonesa com seu caldo rico e ingredientes clássicos."/>
+                <Card imagem={Img3Card} texto="Ramen tradicional"texto2="Traz a autenticidade da culinária japonesa com seu caldo rico e ingredientes clássicos."/>
             </div>
         </div>
     )
